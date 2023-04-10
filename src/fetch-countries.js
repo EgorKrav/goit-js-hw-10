6db58c516184c43cccf802853f9a1874384fdc5f
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-04-10 12:49:45 +0300

    js10hw

--- src/fetch-countries.js
+++ src/fetch-countries.js
@@ -6,3 +6,4 @@ export function fetchCountries(name) {
     .then(response => response.json())
     .catch(error => console.log(error));
 }
+
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Notiflix from 'notiflix';
 import { fetchCountries } from './fetch-countries';
 
 const DEBOUNCE_DELAY = 300;
-
 const countryInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
@@ -17,32 +16,24 @@ countryInput.addEventListener(
 function onCountryInput() {
   const name = countryInput.value.trim();
   if (name === '') {
-    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '');
+    clearCountryElements();
+    return;
   }
 
-  fetchCountries(name)
-    .then(countries => {
-      countryList.innerHTML = '';
-      countryInfo.innerHTML = '';
-      if (countries.length === 1) {
-        countryList.insertAdjacentHTML(
-          'beforeend',
-          renderCountryList(countries)
-        );
-        countryInfo.insertAdjacentHTML(
-          'beforeend',
-          renderCountryInfo(countries)
-        );
-      } else if (countries.length >= 10) {
-        alertTooManyMatches();
-      } else {
-        countryList.insertAdjacentHTML(
-          'beforeend',
-          renderCountryList(countries)
-        );
-      }
-    })
-    .catch(alertWrongName);
+  fetchCountries(name).then(handleFetchResponse).catch(handleFetchError);
+}
+
+function handleFetchResponse(countries) {
+  clearCountryElements();
+
+  if (countries.length === 1) {
+    renderCountryList(countries);
+    renderCountryInfo(countries);
+  } else if (countries.length >= 10) {
+    alertTooManyMatches();
+  } else {
+    renderCountryList(countries);
+  }
 }
 
 function renderCountryList(countries) {
@@ -50,13 +41,13 @@ function renderCountryList(countries) {
     .map(({ name, flags }) => {
       return `
           <li class="country-list__item">
-              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width="30px" height="30px">
               <h2 class="country-list__name">${name.official}</h2>
           </li>
           `;
     })
     .join('');
-  return markup;
+  countryList.innerHTML = markup;
 }
 
 function renderCountryInfo(countries) {
@@ -66,12 +57,23 @@ function renderCountryInfo(countries) {
         <ul class="country-info__list">
             <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
             <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
-            <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${Object.values(
+              languages
+            ).join(', ')}</p></li>
         </ul>
         `;
     })
     .join('');
-  return markup;
+  countryInfo.innerHTML = markup;
+}
+
+function clearCountryElements() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
+
+function handleFetchError() {
+  alertWrongName();
 }
 
 function alertWrongName() {
